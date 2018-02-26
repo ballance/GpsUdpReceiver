315ea03676f03063d6394c731addd8677c85ad18
Chris Ballance <chris@ballance.it>
2018-02-25 21:16:33 -0500

    combined with api project

---
+++ Gps.Api/GpsDataRetriever.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Gps.Api.Models;
+using MySql.Data.MySqlClient;
+
+namespace Gps.Api.Controllers
+{
+    public class GpsDataRetriever
+    {
+        private readonly string _connectionString;
+
+        public GpsDataRetriever(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public GpsCoordinate GetCurrentCoordinates(int tenantId = 1)
+        {
+            var retrievedCoordinate = new GpsCoordinate();
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    var comm = conn.CreateCommand();
+                    comm.CommandText =
+                        "SELECT latitude, longitude, elevation FROM gps_coordinate_log WHERE tenantid = @tenantId ORDER BY time_collected DESC LIMIT 1";
+
+                    comm.Parameters.AddWithValue("@tenantId", 1);
+                    var retrievedCoordinateReader = comm.ExecuteReader();
+                    while (retrievedCoordinateReader.Read())
+                    {
+                        retrievedCoordinate.Lat = Convert.ToDouble(retrievedCoordinateReader.GetString("latitude"));
+                        retrievedCoordinate.Lon = Convert.ToDouble(retrievedCoordinateReader.GetString("longitude"));
+                        retrievedCoordinate.Ele = Convert.ToDouble(retrievedCoordinateReader.GetString("elevation"));
+                    }
+                }
+                catch (Exception e)
+                {
+                    // TODO: Write to database
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+                return retrievedCoordinate;
+            }
+        }
+    }
+}
---
+++ Gps.Api/Models/GpsCoordinate.cs
@@ -0,0 +1,23 @@
+﻿namespace Gps.Api.Models
+{
+    
+    // TODO: Move this to a common shared project
+    public class GpsCoordinate
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public double Ele { get; set; }
+
+        public GpsCoordinate()
+        {
+            Lat = 0;
+            Lon = 0;
+            Ele = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Lat}, {Lon}, {Ele}m";
+        }
+    }
+}
