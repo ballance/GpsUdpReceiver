9c6e37b01de4f42b87c15e37291821d63cef3785
Chris Ballance <chris@ballance.it>
2018-02-25 21:16:33 -0500

    combined with api project

---
+++ Gps.Api/Controllers/GpsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Update;
+using Microsoft.Extensions.Configuration;
+
+namespace Gps.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class GpsController : Controller
+    { 
+        private readonly GpsDataRetriever _gpsDataRetriever;
+
+        public GpsController(IConfiguration configuration)
+        {
+            var connectionString = configuration["ConnectionString"];
+            _gpsDataRetriever = new GpsDataRetriever(connectionString);
+        }
+        // GET api/gps
+        [HttpGet]
+        public string Get()
+        {
+            return _gpsDataRetriever.GetCurrentCoordinates().ToString();
+        }
+        
+        // GET api/gps
+        [HttpGet("{tenantId}")]
+        public string Get(int tenantId)
+        {
+            return _gpsDataRetriever.GetCurrentCoordinates(tenantId).ToString();
+        }
+    }
+}
